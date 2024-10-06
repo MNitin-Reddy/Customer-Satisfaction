d1e9860c2d8066eb834b040dac0fecf4deabba0a
MNitin-Reddy <mandanitinreddy@gmail.com>
2024-10-06 12:26:49 +0530

    Integrated mlflow experimenttracker

--- steps/evaluate.py
+++ steps/evaluate.py
@@ -12,7 +12,12 @@ from typing_extensions import Annotated
 
 from src.evaluation import MSE,RMSE,R2
 
-@step
+from zenml.client import Client
+import mlflow
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker= experiment_tracker.name)
 def evaluate_model(
     model: RegressorMixin,
     X_test: pd.DataFrame,
@@ -33,12 +38,15 @@ def evaluate_model(
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric('mse',mse)
 
         r2_class = R2()
         r2_score = r2_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric('r2',r2_score)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("rmse",rmse)
 
         return r2_score,rmse
     except Exception as e:
--- steps/model_train.py
+++ steps/model_train.py
@@ -11,8 +11,13 @@ from sklearn.base import RegressorMixin
 from steps.config import ModelNameConfig
 
 from zenml import step
+import mlflow
 
-@step
+from zenml.client import Client
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker= experiment_tracker.name)
 def train_model(
     X_train: pd.DataFrame,
     X_test: pd.DataFrame,
@@ -31,6 +36,7 @@ def train_model(
 
         model =None
         if config.model_name == "LinearRegression":
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel()
             trained_model = model.train(X_train,y_train)
             return trained_model
